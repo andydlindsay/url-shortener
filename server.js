faf676ce9597d50479eb2d4b48378302c236712e
Andy Lindsay <andydlindsay@gmail.com>
2017-03-10 19:33:25 +0000

    push to heroku

--- server.js
+++ server.js
@@ -1,8 +1,37 @@
 var express = require('express');
+var path = require('path');
+var utilities = require('./utilities.js');
 
 var app = express();
 
-app.use(express.static(__dirname + '/templates'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+app.use(express.static(path.join(__dirname + '.../templates')));
+app.set('views', path.join(__dirname, '/templates'));
+app.set('css', path.join(__dirname, '/templates/css'));
+
+app.get(['/new/https://:url', '/new/http://:url'], function(req, res) {
+    // console.log('req.params.url:', req.params.url);
+    // check if passed url string is valid
+    console.log(utilities.validURL(req.params.url));
+    
+    // check if http:// or https:// submitted by looking at the 10th character in the request url
+    // console.log(req.url[9] == 's');
+    
+    var responseObj = {
+      original_url: (req.url[9] == 's' ? 'https://' + req.params.url : 'http://' + req.params.url ),
+      short_url: null
+    };
+    res.send(responseObj);
+});
+
+app.get('/new/*', function(req, res) {
+   res.send({ error: "Please enter a valid url containing http:// or https://."}); 
+});
+
+app.get('/:shortened_url_id', function(req, res) {
+    res.send(req.params.shortened_url_id); 
+});
 
 app.get('*', function (req, res) {
     res.render('index');
