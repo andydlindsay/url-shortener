727832403868d87961b0b054884682fcc809a1be
Andy Lindsay <andydlindsay@gmail.com>
2017-03-10 21:14:03 +0000

    fixed css issue

--- server.js
+++ server.js
@@ -6,12 +6,16 @@ var app = express();
 
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
-app.use(express.static(path.join(__dirname + '.../templates')));
+// app.use(express.static(path.join(__dirname + '.../templates')));
+app.use(express.static(__dirname + '/templates'));
 app.set('views', path.join(__dirname, '/templates'));
 app.set('css', path.join(__dirname, '/templates/css'));
 
+console.log('__dirname:', __dirname);
+
 app.get(['/new/https://:url', '/new/http://:url'], function(req, res) {
     // console.log('req.params.url:', req.params.url);
+    
     // check if passed url string is valid
     console.log(utilities.validURL(req.params.url));
     
@@ -19,9 +23,10 @@ app.get(['/new/https://:url', '/new/http://:url'], function(req, res) {
     // console.log(req.url[9] == 's');
     
     var responseObj = {
-      original_url: (req.url[9] == 's' ? 'https://' + req.params.url : 'http://' + req.params.url ),
+      original_url: 'http' + (req.url[9] == 's' ? 's' : '') + '://' + req.params.url,
       short_url: null
     };
+    
     res.send(responseObj);
 });
 
