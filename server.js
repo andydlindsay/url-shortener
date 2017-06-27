bd3d5ae88841a14e15af3d55426dfc842df9b95d
Andy Lindsay <andydlindsay@gmail.com>
2017-06-26 21:53:12 -0700

    adds ability to accept complex url's

--- server.js
+++ server.js
@@ -2,9 +2,20 @@ var express = require('express');
 var path = require('path');
 var utilities = require('./utilities.js');
 var mongo = require('mongodb');
+const favicon = require('serve-favicon');
 var ObjectID = require('mongodb').ObjectID;
 
 var app = express();
+
+// require dotenv to populate environment variables
+require('dotenv').config();
+
+// port number
+const port = process.env.PORT || 8080;
+
+// favicon
+app.use(favicon(path.join(__dirname, 'templates', 'favicon.ico')));
+
 var username = process.env.URLMDBUSER;
 var password = process.env.URLMDBPASS;
 var url = "mongodb://" + username + ":" + password + "@ds127730.mlab.com:27730/url_shortener";
@@ -14,8 +25,8 @@ var db;
 mongo.connect(url, function(err, database) {
     if (err) throw err;
     db = database;
-    app.listen(process.env.PORT, function () {
-        console.log('app listening on port ' + process.env.PORT);
+    app.listen(port, function () {
+        console.log('app listening on port ' + port);
     });
 });
 
@@ -26,13 +37,14 @@ app.use(express.static(__dirname + '/templates'));
 app.set('views', path.join(__dirname, '/templates'));
 // app.set('css', path.join(__dirname, '/templates/css'));
 
-app.get(['/new/https://:url', '/new/http://:url'], function(req, res) {
-    // console.log('req.params.url:', req.params.url);
+app.get(['/new/*'], function(req, res) {
+    const newUrl = req.params[0];
+    console.log('newUrl:', newUrl);
     var responseObj = {};
     
     // check if passed url string is valid
     // console.log(utilities.validURL(req.params.url));
-    if (!(utilities.validURL(req.params.url))) {
+    if (!(utilities.validURL(newUrl))) {
         responseObj = {
            error: "URL must be in the valid http://www.example.com format." 
         };
@@ -41,7 +53,7 @@ app.get(['/new/https://:url', '/new/http://:url'], function(req, res) {
         // check if http:// or https:// submitted by looking at the 10th character in the request url
         // console.log(req.url[9] == 's');
         var secure = (req.url[9] == 's');
-        var orgUrl = req.params.url;
+        var orgUrl = newUrl;
         var shortUrl;
         var docObj = {
             "original_url": orgUrl,
@@ -54,7 +66,7 @@ app.get(['/new/https://:url', '/new/http://:url'], function(req, res) {
             if (err) throw err;
             if (doc.length > 0) {
                 var orgDoc = doc[0];
-                orgUrl = 'http' + (req.url[9] == 's' ? 's' : '') + '://' + orgDoc.original_url;
+                orgUrl = orgDoc.original_url;
                 shortUrl = 'http://andydlindsay-url-shortener.herokuapp.com/' + orgDoc._id;
                 responseObj = {
                   original_url: orgUrl,
@@ -63,9 +75,10 @@ app.get(['/new/https://:url', '/new/http://:url'], function(req, res) {
                 res.send(responseObj);
             } else {
                 // log the url and http type in db and return index to client
+                console.log('docObj:', docObj);
                 db.collection('urls').insert(docObj, function(err, docInserted) {
                     if (err) throw err;
-                    orgUrl = 'http' + (req.url[9] == 's' ? 's' : '') + '://' + req.params.url;
+                    orgUrl = newUrl;
                     shortUrl = 'http://andydlindsay-url-shortener.herokuapp.com/' + docInserted.insertedIds[0];
                     responseObj = {
                       original_url: orgUrl,
@@ -76,10 +89,6 @@ app.get(['/new/https://:url', '/new/http://:url'], function(req, res) {
             }
         });
     }
-});
-
-app.get('/new/*', function(req, res) {
-   res.send({ error: "Url must start with either http:// or https://."}); 
 });
 
 app.get('/:shortened_url_id', function(req, res) {
@@ -101,8 +110,8 @@ app.get('/:shortened_url_id', function(req, res) {
         if (doc.length > 0) {
             // redirect the client
             var orgUrl = doc[0];
-            var builtUrl = 'http' + ( orgUrl.secure ? 's' : '' ) + '://' + orgUrl.original_url;
-            res.redirect(builtUrl);
+            // var builtUrl = orgUrl.original_url;
+            res.redirect(orgUrl.original_url);
         } else {
             // notify the user of db index not found
             res.send({ error: "The supplied url does not match anything in the database." });
